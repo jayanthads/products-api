d7204663fd9b7b683c8122dfd2525807f56f0ee2
jayanthads <65481153+jayanthads@users.noreply.github.com>
2022-10-24 22:43:06 +0530

    Update database.js

--- database.js
+++ database.js
@@ -11,47 +11,28 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         throw err
     } else {
         console.log('Connected to the SQlite database.')
-        db.run(`CREATE TABLE products (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            productName text, 
-            description text,
-            category text,
-            brand text,
-            expiredDate text,
-            manufacturedDate text,
-            batchNumber INTEGER,
-            unitPrice INTEGER,
-            quantity INTEGER,
-            createdDate text
-            )`, (err) => {
-            if (err) {
-                // Table already created
-            } else {
-                // Table just created, creating some rows
-                var insert = 'INSERT INTO products (productName, description, category, brand, expiredDate, manufacturedDate, batchNumber, unitPrice, quantity, createdDate) VALUES (?,?,?,?,?,?,?,?,?,?)'
-                db.run(insert, ["White Basmathi Rice", "White Basmathi Rice imported from Pakistan. High-quality rice with extra fragrance. Organically grown.", "Rice", "CIC", "2023.05.04", "2022.02.20", 324567, , 1020, 200, "2022.02.24"])
-            }
-        })
-
-
-        db.run(`CREATE TABLE suppliers (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            supplierName text, 
+        db.run(`CREATE TABLE customer (
+            customerId INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
             address text,
-            joinedDate text,
-            mobileNo text
+            email text,
+            dateOfBirth text,
+            gender text,       
+            age INTEGER,
+            cardHolderName text,
+            cardNumber INTEGER,
+            expiryDate text,
+            cvv INTERGER,
+            timeStamp text
             )`, (err) => {
             if (err) {
                 // Table already created
             } else {
                 // Table just created, creating some rows
-                var insert = 'INSERT INTO suppliers (supplierName, address, joinedDate, mobileNo) VALUES (?,?,?,?)'
-                db.run(insert, ["D.J.Ishara", "345A ,R.A De Mel Road, Colombo 3", "16/3/2022", "0776600933"])
-
+                var insert = 'INSERT INTO customer (name, address, email, dateOfBirth, gender, age, cardHolderName, cardNumber, expiryDate, cvv,timeStamp) VALUES (?,?,?,?,?,?,?,?,?,?,?)'
+                db.run(insert, ["Jayantha De Silva", "345, Galle Road, Colombo 4", "customer2022@gmail.com", "2000.04.14", "Male", 22, "Laxshman Perera", 102030405060, "12/2023",888,"2022-12-31 23:59:59"])
             }
         })
-
-
 
     }
 })
